3487da070d690e8e5efb9d772b23bbb600eb16fa
Richard Lin <richardqlin@gmail.com>
2016-06-28 21:57:34 -0700

    first version of socket

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+host='127.0.0.1'
+port=12345
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(1)
+conn,addr=s.accept()
+print('connected by',addr)
+while True:
+    data=conn.recv(1024)
+    if not data:break
+    conn.sendall(data)
+conn.close()
